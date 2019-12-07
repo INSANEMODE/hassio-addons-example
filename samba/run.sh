d9e572d9300d10e01c1900ddeff13999cc591bac
INSANEMODE <scottshome02@gmail.com>
2019-12-07 06:12:37 -0600

    change sda1 to sda2

--- samba/run.sh
+++ samba/run.sh
@@ -17,8 +17,8 @@ PASSWORD=$(jq --raw-output '.password // empty' $CONFIG_PATH)
 WAIT_PIDS=()
 NAME=
 
-if [[ -e /dev/sda1 ]]; then
-    mount /dev/sda1 /share/hdd
+if [[ -e /dev/sda2 ]]; then
+    mount /dev/sda2 /share/hdd
 fi
 
 # Check Login data
